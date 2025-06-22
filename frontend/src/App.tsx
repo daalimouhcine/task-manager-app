263440226b8efdddcbed5c853ada3bfdde8ec83e
mouhcine.daali <the.daali.mouhcine@gmail.com>
2025-06-22 13:41:20 +0100

    feat: implement Login page with form handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <>
-      <p className='bg-red-300'>Vite and React</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route
+          path='/'
+          element={
+            <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+              <h1 className='text-4xl font-bold text-gray-800'>Task Manager</h1>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
